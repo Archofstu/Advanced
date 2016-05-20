343244ac15719c3923a4850345cac45bf7a97ac2
cxy921126 <286193150@qq.com>
2016-05-20 21:28:33 +0800

    mac-mini init

---
+++ Advanced/ChartDrawer.swift
@@ -0,0 +1,84 @@
+//
+//  ChartDrawer.swift
+//  Advanced
+//
+//  Created by Mac mini on 16/5/10.
+//  Copyright © 2016年 cxy. All rights reserved.
+//
+
+import UIKit
+import Charts
+import SnapKit
+
+class ChartDrawer: NSObject{
+    
+    static let drawer = ChartDrawer()
+    
+    class func shareDrawer() -> ChartDrawer{
+        return drawer
+    }
+
+    func drawLineChart(inView view : UIView, withXvalues xValues:[String], yValues:[Float]) -> LineChartView{
+        let lineChart = LineChartView()
+        
+        //描述文本
+        lineChart.descriptionText = "Test Line Chart"
+        lineChart.descriptionFont = UIFont.systemFontOfSize(15)
+        //X轴标签位置
+        lineChart.xAxis.labelPosition = .Bottom
+        //动画设置
+        lineChart.animate(xAxisDuration: 1, yAxisDuration: 1)
+        //Y轴最小值
+        //lineChart.leftAxis.axisMinValue = 0
+        //lineChart.rightAxis.axisMinValue = 0
+        //设置边框
+        lineChart.drawBordersEnabled = true
+        
+        var chartDataEntries = [ChartDataEntry]()
+        for i in 0...yValues.count - 1{
+            let dataEntry = ChartDataEntry(value: Double(yValues[i]), xIndex: i)
+            chartDataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = LineChartDataSet(yVals: chartDataEntries, label: "test temperature")
+        
+        //设置端点大小
+        chartDataSet.circleRadius = 3
+        //设置y值显示保留小数点后一位
+        let valueFormatter = NSNumberFormatter()
+        valueFormatter.positiveFormat = "#.0"
+        chartDataSet.valueFormatter = valueFormatter
+        
+        let data = LineChartData(xVals: xValues, dataSet: chartDataSet)
+        
+        lineChart.data = data
+        
+        view.addSubview(lineChart)
+        
+        lineChart.snp_makeConstraints { (make) in
+            make.bottom.equalTo(lineChart.superview!.snp_bottom).offset(-55)
+            make.top.equalTo(lineChart.superview!.snp_top).offset(130)
+            make.leading.equalTo(lineChart.superview!.snp_leading).offset(10)
+            make.trailing.equalTo(lineChart.superview!.snp_trailing).offset(-10)
+        }
+        
+        return lineChart
+    }
+        
+    func refreshNewChart(lineChart:LineChartView, index:Int){
+        let value = Double(arc4random() % 5) + 20.0
+        let entry = ChartDataEntry(value: value, xIndex: index)
+        lineChart.lineData!.dataSets[0].addEntry(entry)
+        lineChart.lineData?.addXValue("xx")
+        //随着点数增加自动调整视图
+        lineChart.autoScaleMinMaxEnabled = true
+        lineChart.setVisibleXRangeMaximum(CGFloat(20))
+        if index>20 {
+            lineChart.moveViewToX(CGFloat(index-20))
+        }
+        
+        lineChart.notifyDataSetChanged()
+        lineChart.invalidateIntrinsicContentSize()
+    }
+
+}
---
+++ Advanced/RealtimeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RealtimeViewController.swift
+//  Advanced
+//
+//  Created by  cxy on 16/5/19.
+//  Copyright © 2016年 cxy. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class RealtimeViewController: UIViewController {
+    var lineChart:LineChartView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let lineChart = ChartDrawer.shareDrawer().drawLineChart(inView: view, withXvalues: ["test"], yValues: [21.2])
+        
+        self.lineChart = lineChart
+        let timer = NSTimer(timeInterval: 1.0, target: self, selector: #selector(realtimeRefresh), userInfo: nil, repeats: true)
+        NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSDefaultRunLoopMode)
+        // Do any additional setup after loading the view.
+    }
+    
+    var index = 1
+    
+    func realtimeRefresh(){
+        ChartDrawer.shareDrawer().refreshNewChart(lineChart!, index: index)
+        index += 1
+    }
+
+}
